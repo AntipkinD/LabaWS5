1041dbaf2efa20c19f6ab029dde319eaa17581ff
mushroomvoodoo <normeeboy@gmail.com>
2024-04-15 10:09:16 +0300

    ..

--- LabaWS5/Program.cs
+++ LabaWS5/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Animal horse = new Horse();
+        Animal horse = new Horse("сено", "стойло", "ФФРФРФРФФРРФРФРРФ");
         horse.makeNoise(horse.Sleep(false));
         new Cat("рыбов", "комната", 1000).Eat();
         Dog sobaka = new Dog("кусок мяса", "дом попа", false);
